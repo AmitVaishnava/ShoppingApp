77494ef26b144965f094d8e6b62db8cf20d10f05
amit <amitvaishnava@gmail.com>
2019-09-22 16:50:18 +0530

    Started product Overview screen

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import productsReducer from "./store/reducers/products";
+
+const rootReducers = combineReducers({
+  products: productsReducer
+});
+
+const store = createStore(rootReducers);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <View><Text>Amit</Text></View>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
