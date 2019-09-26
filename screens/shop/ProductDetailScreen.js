5ae093435855a56e8c6a4f6a9644a1b83a7135f4
amit <amitvaishnava@gmail.com>
2019-09-26 23:47:58 +0530

    Adding items to cart in reducer

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -7,7 +7,8 @@ import {
     Button,
     Image
 } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import * as cartActions from "../../store/actions/cart";
 
 import Colors from "../../constants/Colors";
 
@@ -16,13 +17,15 @@ const ProductDetailScreen = props => {
     const productId = props.navigation.getParam("productId");
     const selectedProduct = useSelector(state =>
         state.products.availableProducts.find(prod => prod.id === productId));
-
+    const dispatch = useDispatch();
 
     return (
         <ScrollView>
             <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
             <View style={styles.actions}>
-                <Button color={Colors.primaryColor} title="Add to cart" onPress={() => { }} />
+                <Button color={Colors.primaryColor} title="Add to cart" onPress={() => {
+                    dispatch(cartActions.addToCart(selectedProduct));
+                }} />
             </View>
             <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
             <Text style={styles.description}>{selectedProduct.description}</Text>
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { FlatList, Text } from "react-native";
-import { useSelector } from "react-redux";
+import { FlatList } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 
 import ProductItem from "../../components/shop/ProductItem";
+import * as cartActions from "../../store/actions/cart";
 
 const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
+    const dispatch = useDispatch();
 
     return (
         <FlatList
@@ -22,7 +24,9 @@ const ProductsOverviewScreen = props => {
                             productTitle: itemData.item.title,
                         });
                     }}
-                    onAddToCart={() => { }} />
+                    onAddToCart={() => {
+                        dispatch(cartActions.addToCart(itemData.item));
+                    }} />
             )} />
     );
 };
